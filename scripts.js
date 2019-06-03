922613c7aa7c30492421cd2dfb6f100159cee5fe
z3t4z00k <z3t4z00k@gmail.com>
2019-06-04 00:47:03 +0700

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,64 @@
+class ToDoClass {
+    constructor() {
+        this.tasks = [
+            { task: 'Go to dentist', isComplete: false },
+            { task: 'Do gardeneing', isComplete: true },
+            { task: 'Renew library account', isComplete: false }
+        ];
+        this.loadTasks();
+    }
+    loadTasks() {
+        let tasksHtml = this.tasks.reduce((html, task, index) => html += this.generateTasksHtml(task, index), '');
+        document.getElementById('taskList').innerHTML = tasksHtml;
+    }
+    generateTasksHtml(task, index) {
+        return `
+            <li class="list-group-item checkbox">
+                <div class="row">
+                    <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 checkbox">
+                        <label><input id="toggleTaskStatus" type="checkbox"  onchange="toDo.toggleTaskStatus(${index})" value="" class="" ${task.isComplete?'checked':''}></label>
+                    </div>
+                    <div class="col-md-10 col-xs-10 col-lg-10 col-sm-10 task-text ${task.isComplete?'complete':''}">
+                        ${task.task}
+                    </div>
+                    <div class="col-md-1 col-xs-1 col-lg-1 col-sm-1 delete-icon-area">
+                        <a class="" href="/toDoList" onclick="toDo.deleteTask(event, ${index})"><i id="deleteTask" data-id="${index}" class="delete-icon glyphicon glyphicon-trash"></i></a>
+                    </div>
+                </div>
+            </li>
+        `;
+    }
+    toggleTaskStatus(index) {
+        this.tasks[index].isComplete = !this.tasks[index].isComplete;
+        this.loadTasks();
+    }
+    deleteTask(event, taskIndex) {
+        event.preventDefault();
+        this.tasks.splice(taskIndex, 1);
+        this.loadTasks();
+    }
+    addTaskClick() {
+        let target = document.getElementById('addTask');
+        this.addTask(target.value);
+        target.value = "";
+    }
+    addTask(task) {
+        let newTask = {
+            task,
+            isComplete: false,
+        };
+        let parentDiv = document.getElementById('addTask').parentElement;
+        if (task === '')
+            parentDiv.classList.add('has-error');
+        else {
+            parentDiv.classList.remove('has-error');
+            this.tasks.push(newTask);
+            this.loadTasks();
+        }
+    }
+}
+
+let toDo;
+window.addEventListener("load", () => { //let a = function(x){} === let a = x => {}
+    toDo = new ToDoClass();
+})
